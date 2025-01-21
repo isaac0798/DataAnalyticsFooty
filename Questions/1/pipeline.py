91b002629ddeac093905810d3581dadfb55fd980
Isaac Tennant <isaactennant@isaacs-mbp.home>
2025-01-21 09:58:22 +0000

    migrating to supabase

--- Questions/1/pipeline.py
+++ Questions/1/pipeline.py
@@ -2,6 +2,10 @@ import sys
 import os
 import json
 import sqlite3
+from supabase import create_client, Client
+from dotenv import load_dotenv, dotenv_values 
+# loading variables from .env file
+load_dotenv()
 
 args = sys.argv[1:]
 print(args[0])
@@ -9,101 +13,74 @@ print(args[0])
 json_file = open(args[0])
 match_data = json.load(json_file)
 
-# Connect to the db
-with sqlite3.connect('./Questions/1/example.db') as conn:
-    cursor = conn.cursor()
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS event (
-            id INTEGER PRIMARY KEY,
-            uuid TEXT NOT NULL,
-            type_id INTEGER NOT NULL,
-            player_id INTEGER NOT NULL,
-            timestamp TEXT NOT NULL,
-            location_x REAL NOT NULL,
-            location_y REAL NOT NULL,
-            end_location_x REAL NOT NULL,
-            end_location_y REAL NOT NULL
-        );
-    ''')
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS players (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL
-        );
-    ''')
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS type (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL
-        );
-    ''')
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
     
-    conn.commit()
+for event in match_data:
+    if 'player' not in event:
+        continue
     
-    for event in match_data:
-        if 'player' not in event:
-            continue
+    if 'location' not in event:
+        continue
+
+    eventType = event['type']['name'].lower()
+
+    response = (
+      supabase.table("players")
+        .upsert({"id": event['player']['id'], "name": event['player']['name']}, on_conflict="id")
+        .execute()
+    )
+
+    response = (
+      supabase.table("type")
+        .upsert({"id": event['type']['id'], "name": event['type']['name']}, on_conflict="id")
+        .execute()
+    )
+    """ 
+
+    cursor.execute('''
+      INSERT OR IGNORE INTO type (id, name)
+      VALUES (?, ?)
+    ''', (
+      event['type']['id'],
+      event['type']['name']
+    ))
+
+    if eventType not in event:
+        print(f'cannot find event details: {eventType}')
+        cursor.execute('''
+            INSERT INTO event (uuid, type_id, player_id, timestamp, location_x, location_y, end_location_x, end_location_y)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (
+            event['id'],
+            event['type']['id'],
+            event['player']['id'],
+            event['timestamp'],
+            event['location'][0],
+            event['location'][1],
+            event['location'][0],
+            event['location'][1]  
+        ))
+    else:
+        print('found event')
+        end_location_x = event['location'][0]
+        end_location_y = event['location'][1] 
         
-        if 'location' not in event:
-            continue
-
-        eventType = event['type']['name'].lower()
-
+        if eventType in event and 'end_location' in event[eventType]:
+            end_location_x = event[eventType]['end_location'][0]
+            end_location_y = event[eventType]['end_location'][1]
+        
         cursor.execute('''
-          INSERT OR IGNORE INTO players (id, name)
-          VALUES (?, ?)
+            INSERT INTO event (uuid, type_id, player_id, timestamp, location_x, location_y, end_location_x, end_location_y)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         ''', (
-          event['player']['id'],
-          event['player']['name']
-        ))
-
-        cursor.execute('''
-          INSERT OR IGNORE INTO type (id, name)
-          VALUES (?, ?)
-        ''', (
-          event['type']['id'],
-          event['type']['name']
-        ))
-
-        if eventType not in event:
-            print(f'cannot find event details: {eventType}')
-            cursor.execute('''
-                INSERT INTO event (uuid, type_id, player_id, timestamp, location_x, location_y, end_location_x, end_location_y)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            ''', (
-                event['id'],
-                event['type']['id'],
-                event['player']['id'],
-                event['timestamp'],
-                event['location'][0],
-                event['location'][1],
-                event['location'][0],
-                event['location'][1]  
-            ))
-        else:
-            print('found event')
-            end_location_x = event['location'][0]
-            end_location_y = event['location'][1] 
-            
-            if eventType in event and 'end_location' in event[eventType]:
-                end_location_x = event[eventType]['end_location'][0]
-                end_location_y = event[eventType]['end_location'][1]
-            
-            cursor.execute('''
-                INSERT INTO event (uuid, type_id, player_id, timestamp, location_x, location_y, end_location_x, end_location_y)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-            ''', (
-                event['id'],
-                event['type']['id'],
-                event['player']['id'],
-                event['timestamp'],
-                event['location'][0],
-                event['location'][1],
-                end_location_x,
-                end_location_y
-            ))
-
-cursor.close()
+            event['id'],
+            event['type']['id'],
+            event['player']['id'],
+            event['timestamp'],
+            event['location'][0],
+            event['location'][1],
+            end_location_x,
+            end_location_y
+        ))  """
--- Questions/Questions.md
+++ Questions/Questions.md
@@ -3,6 +3,6 @@ The aim of this folder is to be more direct with the dataset come up with questi
 1 - data -> sql table -> web
   a - take each event and put it an event table ✅
   b - each player should be in an own table ✅
-  c - each type should be in its own table 
+  c - each type should be in its own table ✅
   d - build interface to pick an event, can filter table on event
   e - generate image of event
